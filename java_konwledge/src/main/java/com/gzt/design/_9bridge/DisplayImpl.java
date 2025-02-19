939151296c35ea5445add4c7163ecb9bf7edfbb1
kwok <18875914054@163.com>
2025-02-19 23:16:40 +0800

    feat: 桥接模式

---
+++ java_konwledge/src/main/java/com/gzt/design/_9bridge/DisplayImpl.java
@@ -0,0 +1,7 @@
+package com.gzt.design._9bridge;
+
+public abstract class DisplayImpl {
+    public abstract void rawOpen();
+    public abstract void rawPrint();
+    public abstract void rawClose();
+}
---
+++ java_konwledge/src/test/java/design/_9bridge/BridgeTest.java
@@ -0,0 +1,31 @@
+package design._9bridge;
+
+import com.gzt.design._9bridge.CountDisplay;
+import com.gzt.design._9bridge.Display;
+import com.gzt.design._9bridge.StringDisplayImpl;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BridgeTest {
+
+    /**
+     * 1.Abstraction(抽象化)-Display
+     * 该角色位于“类的功能层次结构”的最上层。它使用Implementor角色的方法定义了基本的功能。该角色中保存了Implementor角色的实例。
+     * 2.RefinedAbstraction(改善后的抽象化)-CountDisplay
+     * 在 Abstraction角色的基础上增加了新功能的角色。
+     * 3.Implementor(实现者)-DisplayImpl
+     * 该角色位于“类的实现层次结构”的最上层。它定义了用于实现Abstraction角色的接口(API)的方法。
+     * 4.Concretelmplementor(具体实现者)-StringDisplayImpl
+     * 该角色负责实现在 Implementor角色中定义的接口(API)
+     */
+    @Test
+    public void test() {
+        Display d1 = new Display(new StringDisplayImpl("Hello, world."));
+        Display d2 = new CountDisplay(new StringDisplayImpl("Hello, world with today."));
+        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+        d1.display();
+        d2.display();
+        d3.multiDisplay(5);
+    }
+}
